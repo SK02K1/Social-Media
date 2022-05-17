996f77133848741af5bd099d49ab4588470fec38
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-05-17 13:45:02 +0530

    add: chakra container component to center content

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import { Box, useColorModeValue } from '@chakra-ui/react';
+import { Box, Container, useColorModeValue } from '@chakra-ui/react';
 import { Navbar, RequiresAuth } from 'components';
 import { Posts, Login, Signup } from 'app/features';
 
@@ -7,16 +7,18 @@ export const App = () => {
   const appBg = useColorModeValue('#F7FAFC', '#1A202C');
   return (
     <Box minH='100vh' bg={appBg} className='App'>
-      <Navbar />
-      <Routes>
-        {/* Public Routes */}
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        {/* Private Routes */}
-        <Route element={<RequiresAuth />}>
-          <Route path='/' element={<Posts />} />
-        </Route>
-      </Routes>
+      <Container maxW='container.xl'>
+        <Navbar />
+        <Routes>
+          {/* Public Routes */}
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          {/* Private Routes */}
+          <Route element={<RequiresAuth />}>
+            <Route path='/' element={<Posts />} />
+          </Route>
+        </Routes>
+      </Container>
     </Box>
   );
 };
