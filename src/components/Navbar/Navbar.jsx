5a5921992f55a9e8207eebebcba21d6830683d29
SK02K1 <ksourabh458@gmail.com>
2022-05-18 23:27:02 +0530

    remove: logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import {
   Container,
+  Heading,
   HStack,
   IconButton,
-  Image,
   useColorMode,
   useColorModeValue,
 } from '@chakra-ui/react';
 
 export const Navbar = () => {
-  const { toggleColorMode, colorMode } = useColorMode();
+  const { toggleColorMode } = useColorMode();
   const themeIcon = useColorModeValue(<MoonIcon />, <SunIcon />);
   const navbarBg = useColorModeValue(
     'rgb(247, 250, 252, 0.8)',
@@ -28,12 +28,7 @@ export const Navbar = () => {
       bg={navbarBg}
     >
       <HStack as='nav' justifyContent='space-between' alignItems='center'>
-        <Image
-          width='10rem'
-          src={`/assets/logo-${colorMode}.svg`}
-          alt='share moment logo'
-        />
-
+        <Heading>LOGO</Heading>
         <IconButton
           onClick={toggleColorMode}
           variant='outline'
